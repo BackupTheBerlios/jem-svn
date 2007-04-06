daa8b757dea378d9bd3b9fa56fb69aab64fe99da
mranon <mranon@993cd96a-242c-0410-a145-8c1a13d7531a>
2007-04-06 03:05:39 +0000

    Add zero_CPUState.c

--- kjvm/trunk/dom0/zero_CPUState.c
+++ kjvm/trunk/dom0/zero_CPUState.c
@@ -1,4 +1,48 @@
-#include "all.h"
+//=================================================================================
+// This file is part of Jem, a real time Java operating system designed for 
+// embedded systems.
+//
+// Copyright © 2007 JemStone Software LLC. All rights reserved.
+// Copyright © 1997-2001 The JX Group. All rights reserved.
+//
+// Jem is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License, version 2, as published by the Free 
+// Software Foundation.
+//
+// Jem is distributed in the hope that it will be useful, but WITHOUT ANY 
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with 
+// Jem; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, 
+// Fifth Floor, Boston, MA 02110-1301, USA
+//
+//==============================================================================
+// zero_CPUState.c
+// 
+// 
+//==============================================================================
+
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <native/mutex.h>
+#include <native/task.h>
+#include <native/event.h>
+#include "jemtypes.h"
+#include "jemConfig.h"
+#include "object.h"
+#include "domain.h"
+#include "gc.h"
+#include "code.h"
+#include "portal.h"
+#include "thread.h"
+#include "vmsupport.h"
+#include "load.h"
+#include "exception_handler.h"
+#include "zero.h"
+#include "malloc.h"
+#include "gc_alloc.h"
 
 extern ClassDesc *cpuStateClass;
 extern ClassDesc *stackClass;
@@ -7,51 +51,44 @@ extern ClassDesc *domainZeroClass;
 
 static jint cpuState_getState(ObjectDesc * self)
 {
-	ThreadDesc *thread = cpuState2thread(self);
-	ASSERTTHREAD(thread);
-	return thread->state;
+    ThreadDesc *thread = cpuState2thread(self);
+    return thread->state;
 }
 static jboolean cpuState_isPortalThread(ObjectDesc * self)
 {
-	ThreadDesc *thread = cpuState2thread(self);
-	ASSERTTHREAD(thread);
-	return thread->isPortalThread;
+    ThreadDesc *thread = cpuState2thread(self);
+    return thread->isPortalThread;
 }
 static ObjectDesc *cpuState_setNext(ObjectDesc * self, ObjectDesc * next)
 {
-	ObjectDesc *result;
-	ThreadDesc *thread = cpuState2thread(self);
-	ASSERTTHREAD(thread);
-	if (thread == NULL)
-		return NULL;
-	result = thread2CPUState(thread->next);
-	ASSERTTHREAD(thread->next);
+    ObjectDesc *result;
+    ThreadDesc *thread = cpuState2thread(self);
+    if (thread == NULL)
+        return NULL;
+    result = thread2CPUState(thread->next);
 
-	thread->next = cpuState2thread(next);
-	ASSERTTHREAD(thread->next);
-	return result;
+    thread->next = cpuState2thread(next);
+    return result;
 }
 static ObjectDesc *cpuState_getNext(ObjectDesc * self)
 {
-	ObjectDesc *result;
-	ThreadDesc *thread = cpuState2thread(self);
-	ASSERTTHREAD(thread);
-	if (thread == NULL)
-		return NULL;
-	result = thread2CPUState(thread->next);
-	ASSERTTHREAD(thread->next);
-	return result;
+    ObjectDesc *result;
+    ThreadDesc *thread = cpuState2thread(self);
+    if (thread == NULL)
+        return NULL;
+    result = thread2CPUState(thread->next);
+    return result;
 }
 
 MethodInfoDesc cpuStateMethods[] = {
-	{"getState", "", (code_t) cpuState_getState}
-	,
-	{"isPortalThread", "", (code_t) cpuState_isPortalThread}
-	,
-	{"setNext", "", (code_t) cpuState_setNext}
-	,
-	{"getNext", "", (code_t) cpuState_getNext}
-	,
+    {"getState", "", (code_t) cpuState_getState}
+    ,
+    {"isPortalThread", "", (code_t) cpuState_isPortalThread}
+    ,
+    {"setNext", "", (code_t) cpuState_setNext}
+    ,
+    {"getNext", "", (code_t) cpuState_getNext}
+    ,
 };
 
 static jbyte cpuStateTypeMap[] = { 0, 0 };
@@ -59,14 +96,9 @@ static jbyte cpuStateTypeMap[] = { 0, 0 };
 MethodInfoDesc stackMethods[] = {
 };
 
-static jbyte stackTypeMap[] = { 0, 0 };
-
-void init_cpustate_portal()
+void init_cpustate_portal(void)
 {
-	cpuStateClass =
-	    init_zero_class("jx/zero/CPUState", cpuStateMethods, sizeof(cpuStateMethods), 1, cpuStateTypeMap,
-			    "<jx/zero/CPUState>");
-#ifdef STACK_ON_HEAP
-	stackClass = init_zero_class("jx/zero/Stack", stackMethods, sizeof(stackMethods), 1, stackTypeMap, "<jx/zero/Stack>");
-#endif
+    cpuStateClass =
+        init_zero_class("jx/zero/CPUState", cpuStateMethods, sizeof(cpuStateMethods), 1, cpuStateTypeMap,
+                "<jx/zero/CPUState>");
 }
